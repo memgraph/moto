0a6f94d1bab8b4af51bf544d7dcbc40f60d32327
Vinko Kasljevic <vinko.kasljevic@memgraph.io>
2019-06-28 09:23:21 +0200

    Update init_base.sh script

--- moto/vm_scripts/init_base.sh
+++ moto/vm_scripts/init_base.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
+# Arg 1 - Url to zipped base image
+#   Extracted base image must be named 'base.vdi'
+#   Base image must have Virtualbox guest additions
+# Arg 2 - Network interface to use for virtualbox instances
 
 # Download base.vdi
-# Imange must have Virtualbox guest additions
 curl $1 > base.zip
 unzip base.zip
 rm base.zip
-
-# Create host only network
-net=$(vboxmanage hostonlyif create 2> /dev/null  | sed -E "s/.*Interface '(.*)'.*/\1/")
-vboxmanage hostonlyif ipconfig $net --ip 10.10.10.1
-vboxmanage dhcpserver add --ifname $net --ip 10.10.10.1 --netmask 255.255.255.0 --lowerip 10.10.10.10 --upperip 10.10.10.254
-vboxmanage dhcpserver modify --ifname $net --enable
 
 # Create base virtualbox intance
 vboxmanage createvm --name base --ostype Linux_64 --register
 vboxmanage modifyvm base --memory 512 --vram 16
 vboxmanage modifyvm base --graphicscontroller vmsvga
-vboxmanage modifyvm base --hostonlyadapter1 $net --nic1 hostonly
+vboxmanage modifyvm base --nic1 bridged --bridgeadapter1 $2
 vboxmanage storagectl base --name "IDE Controller" --add ide
 vboxmanage storageattach base --storagectl "IDE Controller" --port 0 --device 0 --type hdd --medium base.vdi
