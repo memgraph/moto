3b5ac6ed434454f4eb53b6ce880bcff874301892
Vinko Kasljevic <vinko.kasljevic@memgraph.io>
2019-07-01 16:48:08 +0200

    Add VM_SCRIPT_DIR to settings.py

--- moto/settings.py
+++ moto/settings.py
@@ -1,4 +1,4 @@
 import os
-
 TEST_SERVER_MODE = os.environ.get('TEST_SERVER_MODE', '0').lower() == 'true'
 RUN_AS_VM = False
+VM_SCRIPT_DIR = os.path.abspath(os.path.dirname(__file__)) + '/vm_scripts'
