4fbe73fd8f3702be3c82b7b39f32b97ef1609dcf
Vinko Kasljevic <vinko.kasljevic@memgraph.io>
2019-07-01 16:49:05 +0200

    Make get_ip.sh more robust

--- moto/vm_scripts/get_ip.sh
+++ moto/vm_scripts/get_ip.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
-vboxmanage guestproperty get $1 "/VirtualBox/GuestInfo/Net/0/V4/IP" | cut -d" " -f 2
+
+if [ -z $1 ]; then
+    echo "Instance name missing"
+    exit 1
+fi
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+out=$(bash $DIR/instance_bash.sh $1 "ip -4 addr show eth0 | grep inet | sed -E 's/.*inet (.*)\/.*/\1/'" 2> /dev/null)
+# In case instance is not running or network interface in not running or output is empty
+if [ $? != 0 ] || [ -z $out ]; then
+    exit 1
+fi
+
+echo $out
