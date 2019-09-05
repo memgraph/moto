74e88b0317a326156d4aa99103e4a0a6029138d0
Vinko Kasljevic <vinko.kasljevic@memgraph.io>
2019-09-05 09:43:10 +0200

    Migrate to debian image

--- moto/vm_scripts/get_ip.sh
+++ moto/vm_scripts/get_ip.sh
@@ -6,7 +6,8 @@ if [ -z $1 ]; then
 fi
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-out=$(bash $DIR/instance_bash.sh $1 "ip -4 addr show eth0 | grep inet | sed -E 's/.*inet (.*)\/.*/\1/'" 2> /dev/null)
+out=$(bash $DIR/instance_bash.sh $1 "ip -4 addr show enp0s3 | grep inet | sed -E 's/.*inet (.*)\/.*/\1/'" 2> /dev/null)
+
 # In case instance is not running or network interface in not running or output is empty
 if [ $? != 0 ] || [ -z $out ]; then
     exit 1
--- moto/vm_scripts/init_base.sh
+++ moto/vm_scripts/init_base.sh
@@ -15,7 +15,7 @@ rm base.zip
 
 # Create host only network
 net=$(vboxmanage hostonlyif create 2> /dev/null  | sed -E "s/.*Interface '(.*)'.*/\1/")
-if [ $net -eq 'vboxnet0' ]; then
+if [ $net != 'vboxnet0' ]; then
     echo "Host only network might already exist, please check networks in VirtualBox"
 fi
 
@@ -24,9 +24,9 @@ vboxmanage dhcpserver add --ifname $net --ip 10.10.10.1 --netmask 255.255.255.0 
 vboxmanage dhcpserver modify --ifname $net --enable
 
 # Create base virtualbox intance
-vboxmanage createvm --name base --ostype Linux_64 --register
+vboxmanage createvm --name base --ostype Ubuntu_64 --register
 vboxmanage modifyvm base --memory 512 --vram 16
 vboxmanage modifyvm base --graphicscontroller vmsvga
 vboxmanage modifyvm base --hostonlyadapter1 $net --nic1 hostonly
-vboxmanage storagectl base --name "IDE Controller" --add ide
-vboxmanage storageattach base --storagectl "IDE Controller" --port 0 --device 0 --type hdd --medium base.vdi
+vboxmanage storagectl base --name "SATA" --add sata --portcount 1
+vboxmanage storageattach base --storagectl "SATA" --port 0 --device 0 --type hdd --medium $1
